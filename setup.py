7d93fd0dd0aefabf16ca340b208b54c4c02d4a3d
Lars Kellogg-Stedman <lars@seas.harvard.edu>
2012-04-05 13:47:42 -0400

    prep for packaging

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "zmqevt",
+    author = 'Lars Kellogg-Stedman',
+    author_email = 'lars@seas.harvard.edu',
+    version = "1",
+    packages = find_packages(),
+    scripts = [ 'bin/evtbroker', 'bin/evtsub', 'bin/evtpub' ],
+)
