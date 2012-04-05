120a795268b81a8c824f9e70815efa59e3609d49
Lars Kellogg-Stedman <lars@seas.harvard.edu>
2012-04-05 16:24:57 -0400

    inc version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name = "zmqevt",
     author = 'Lars Kellogg-Stedman',
     author_email = 'lars@seas.harvard.edu',
-    version = "1",
+    version = "2",
     packages = find_packages(),
     scripts = [ 'bin/evtbroker', 'bin/evtsub', 'bin/evtpub',
         'bin/irssi-notify-modem',],
