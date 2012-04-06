3caaef280ea22a34eb8c594eaf3e046889c38c37
Lars Kellogg-Stedman <lars@seas.harvard.edu>
2012-04-06 13:47:00 -0400

    make blinker actually blink when activated
    instead of just turning the modem lights on
    we now start a thread to blink them while
    a notification is active.

--- zmqevt/blinker.py
+++ zmqevt/blinker.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python
 
 import logging
+import threading
+import time
 
 class Blinker (object):
     def __init__ (self, path):
@@ -9,15 +11,26 @@ class Blinker (object):
         self.log = logging.getLogger('zmqevt.blinker')
         self.log.info('Blinker created on %s' % self.path)
 
+        self.on_interval=1
+        self.off_interval=0.5
+
     def on(self):
         if not self.active:
             self.log.info('Activate blinker.')
-            self.fd = open(self.path)
             self.active = True
+            self.t = threading.Thread(target=self.blink)
+            self.t.start()
+
+    def blink(self):
+        while self.active:
+            self.fd = open(self.path)
+            time.sleep(self.on_interval)
+            self.fd.close()
+            time.sleep(self.off_interval)
 
     def off(self):
         if self.active:
             self.log.info('Deactivate blinker.')
-            self.fd.close()
             self.active = False
+            self.t.join()
 
